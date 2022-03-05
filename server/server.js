dc2441591b77f58668d791e2d420e0da86ee6759
C-atfish <72621326+C-atfish@users.noreply.github.com>
2022-03-05 18:08:36 +0100

    Fixed start script for working heroku release

--- server/server.js
+++ server/server.js
@@ -8,6 +8,6 @@ app.use(express.static("../client/dist"))
 
 
 
-const server = app.listen(process.env.port || 3000, ()=> {
+const server = app.listen(process.env.PORT || 3000, ()=> {
     console.log("Server started at port: " + server.address().port)
 })
